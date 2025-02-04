1955208cfbd702ac936cdf4bb3c0d62181d89359
amunns1929 <amunns1929@gmail.com>
2025-02-04 15:09:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+#define DUTY_CW 0.065
+#define DUTY_CCW 0.085
+#define DUTY_STOP 0.075
+#define PERIOD 0.02 //Units of period is seconds
+
+PwmOut servo(PF_9); //Here is where we declare the object
+
 int main()
 {
-   DigitalIn gasDetector(D2);
-   DigitalIn overTempDetector(D3);
-   
-   DigitalOut alarmLed(LED1);
-   
-   gasDetector.mode(PullDown);   
-   overTempDetector.mode(PullDown);
-
-   while (true) {
-      
-      if ( gasDetector || overTempDetector ) {
-         alarmLed = ON;
-      } else {
-         alarmLed = OFF;
-      }
-   }
+servo.period(PERIOD); // 20ms period
+servo.write(DUTY_STOP); // 2.5% duty cycle
+    while(true){
+        servo.write(DUTY_CW);
+        delay(2000);
+        servo.write(DUTY_STOP);
+        delay(3000);
+        servo.write(DUTY_CCW);
+        delay(2000);
+        servo.write(DUTY_STOP);
+        delay(3000);
+    }
 }
